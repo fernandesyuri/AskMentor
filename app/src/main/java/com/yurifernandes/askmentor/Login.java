2c0eb92d121683c314574a6f73b981d680703bd5
Yuri Fernandes <contato@yurifernandes.com>
2019-04-30 11:49:28 -0300

    Downgrade gradle e add aws

--- app/src/main/java/com/yurifernandes/askmentor/Login.java
+++ app/src/main/java/com/yurifernandes/askmentor/Login.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.amazonaws.regions.Regions;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -13,14 +18,32 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class Login extends Activity {
+public class Login extends Activity implements View.OnClickListener {
+
+    private AWSAppSyncClient mAWSAppSyncClient;
+
     private CallbackManager callbackManager;
     private LoginButton loginButton;
+    private Button btnLogin;
+
+    private CognitoCachingCredentialsProvider credentialsProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
+
+        // Inicializar o provedor de credenciais do Amazon Cognito
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-east-1:2d957a98-f39c-40f7-bcaa-f7ef27ec4566", // ID do grupo de identidades
+                Regions.US_EAST_1 // Região
+        );
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -48,16 +71,16 @@ public class Login extends Activity {
                 Toast.makeText(Login.this, exception.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+
+        btnLogin = findViewById(R.id.button1);
     }
 
-    public void callActivity()
-    {
+    public void callActivity() {
         Intent intent = new Intent(Login.this, Teste.class);
         startActivity(intent);
     }
 
-    public boolean isLoggedIn()
-    {
+    public boolean isLoggedIn() {
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         return accessToken != null;
     }
@@ -68,9 +91,15 @@ public class Login extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void onClickFacebookButton(View view)
-    {
-        if(view == loginButton)
+    public void onClickFacebookButton(View view) {
+        if (view == loginButton)
             loginButton.performClick();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnLogin) {
+
+        }
     }
 }
