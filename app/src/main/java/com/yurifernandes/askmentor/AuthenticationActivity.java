1a2add1d05f293bd85a36c4402e779be094afa2e
Yuri Fernandes <contato@yurifernandes.com>
2019-06-11 11:13:44 -0300

    Login com tela de teste

---
+++ app/src/main/java/com/yurifernandes/askmentor/AuthenticationActivity.java
@@ -0,0 +1,78 @@
+package com.yurifernandes.askmentor;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.SignInUIOptions;
+import com.amazonaws.mobile.client.UserStateDetails;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    private final String TAG = AuthenticationActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                Log.i(TAG, userStateDetails.getUserState().toString());
+                switch (userStateDetails.getUserState()) {
+                    case SIGNED_IN:
+                        Intent i = new Intent(AuthenticationActivity.this, Home.class);
+                        startActivity(i);
+                        break;
+                    case SIGNED_OUT:
+                        showSignIn();
+                        break;
+                    default:
+                        AWSMobileClient.getInstance().signOut();
+                        showSignIn();
+                        break;
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        });
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i(TAG, "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e(TAG, "printHashKey()", e);
+        }
+    }
+
+    private void showSignIn() {
+        try {
+            AWSMobileClient.getInstance().showSignIn(this,
+                    SignInUIOptions.builder().nextActivity(Home.class).build());
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+}
