fa0d83043b761e6a9b3b6ac209164ce62e5a1f2f
Otavio <otaviopalma@gmail.com>
2019-06-27 12:29:26 -0300

    Merge dev-otavio

--- app/src/main/java/com/yurifernandes/askmentor/Home.java
+++ app/src/main/java/com/yurifernandes/askmentor/Home.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -256,8 +257,9 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
             Log.v("###BtnPress", "btnQuestion");
             mutation();
         }
-        if(v == camera) {
+        if (v == camera) {
             Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
             startActivity(intent);
         }
     }
+}
--- app/src/main/java/com/yurifernandes/askmentor/Login.java
+++ app/src/main/java/com/yurifernandes/askmentor/Login.java
@@ -3,6 +3,7 @@ package com.yurifernandes.askmentor;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
